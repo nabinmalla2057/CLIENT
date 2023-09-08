a6cb3ddd8a622d3419088f9a2258b9d910932a5d
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-08 23:34:50 +0545

    subtaskcont V2

---
+++ modules/subtasks/subtask.controller.js
@@ -0,0 +1,11 @@
+const Model = require("./subtask.model");
+
+const create = async (payload) => {
+  return await Model.create(payload);
+};
+
+const updateStatus = async (id, status) => {
+  return await Model.findOneAndUpdate({ _id: id }, status, { new: true });
+};
+
+module.exports = { create, list, updateStatus };
