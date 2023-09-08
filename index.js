b042c8fd6e3d4b8bb2e189799716c1f7d94c4e3f
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-08 09:32:31 +0545

    todov2

---
+++ index.js
@@ -0,0 +1,17 @@
+const e = require("express");
+const express = require("express");
+const app = express();
+const port = +process.env.PORT || 3000;
+const indexRouter = require("./routes");
+
+app.use(express.json());
+
+app.use("/", indexRouter);
+app.use((err, res, req, next) => {
+  err = err.toString() || "Something went wrong...";
+  res.status(500).json({ data: "", msg: err });
+});
+
+app.listenerCount(port, () => {
+  console.log("App is running on port:", port);
+});
